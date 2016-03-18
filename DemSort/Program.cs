4f2ad98d8f7f660ce534ae4d0579dc9f138d287f
Manick <manik713@hotmail.com>
2016-03-18 12:41:49 +1000

    Initial Version
    Sorts scores in a file and creates an output file. 
    Records in format : LASTNAME, FIRSTNAME, SCORE

---
+++ DemSort/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DemSort
+{
+    public static class Program
+    {
+        public static IEnumerable<string> SortedContents { get; set; }
+
+        private static void Main(string[] args)
+        {
+            if (args[0] == null) return;
+            var contents = ReadFile(args[0]);
+            if(contents == null || !contents.Any()) return;
+            if (Sort(contents))
+            {
+                WriteToFile(Path.Combine(Path.GetDirectoryName(args[0]), "sorted_score.txt"), SortedContents);
+            }
+            else
+            {
+                Console.WriteLine("Error: Incorrect input format !");
+            }
+        }
+
+        private static IEnumerable<string> ReadFile(string inputFilePath)
+        {
+            var lines = new List<string>();
+            if (File.Exists(inputFilePath))
+            {
+                using (var sr = new StreamReader(inputFilePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+                    }
+                }                
+            }
+            else
+            {
+                Console.WriteLine("Error: The file {0} doesn't exist\n", inputFilePath);                
+            }
+            return lines;
+        }
+
+        public static bool Sort(IEnumerable<string> contents)
+        {
+            var delimiter = new[] { ',' };            
+            try
+            {                
+                SortedContents = contents.Select(x => x.Trim().Split(delimiter).ToArray())
+                .OrderByDescending(x => x[2])
+                .ThenBy(x => x[0])
+                .ThenBy(x => x[1])
+                .Select(x => string.Join(",", x)).ToList();
+                return true;
+            }
+            catch (IndexOutOfRangeException)
+            {                
+                return false;
+            }                                         
+        }
+
+        private static void WriteToFile(string outputFilePath, IEnumerable<string> contents)
+        {
+            if (File.Exists(outputFilePath))
+            {
+                File.Delete(outputFilePath);
+            }
+            using (var sw = new StreamWriter(outputFilePath))
+            {
+                foreach (var content in contents)
+                {
+                    sw.WriteLine(content);
+                }
+            }
+            Console.WriteLine("Finished: Created {0}", outputFilePath);
+        }
+    }
+}
---
+++ SortTest/SortTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SortTest
+{
+    [TestClass]
+    public class SortTest
+    {
+        [TestMethod]
+        public void Test_Sorting_Proper_Format_Order_1()
+        {            
+            IEnumerable<string> contents = new List<string> { "PALMER, TERESSA, 88", "SMITH, ALLAN, 70", "SMITH, FRANCIS, 85", "BUNDY, MADISON, 88" };                                                            
+            Assert.IsTrue(DemSort.Program.Sort(contents));
+            var sortedContents = DemSort.Program.SortedContents.ToList();
+            Assert.AreEqual(sortedContents[0], "BUNDY, MADISON, 88");
+            Assert.AreEqual(sortedContents[1], "PALMER, TERESSA, 88");
+            Assert.AreEqual(sortedContents[2], "SMITH, FRANCIS, 85");
+            Assert.AreEqual(sortedContents[3], "SMITH, ALLAN, 70");            
+        }
+
+        [TestMethod]
+        public void Test_Sorting_Proper_Format_Order_2()
+        {
+            IEnumerable<string> contents = new List<string> { "SMITH, ALLAN, 70", "SMITH, FRANCIS, 85", "BUNDY, TERESSA, 88", "BUNDY, MADISON, 88" };
+            Assert.IsTrue(DemSort.Program.Sort(contents));
+            var sortedContents = DemSort.Program.SortedContents.ToList();
+            Assert.AreEqual(sortedContents[0], "BUNDY, MADISON, 88");
+            Assert.AreEqual(sortedContents[1], "BUNDY, TERESSA, 88");
+            Assert.AreEqual(sortedContents[2], "SMITH, FRANCIS, 85");
+            Assert.AreEqual(sortedContents[3], "SMITH, ALLAN, 70");
+        }
+
+        [TestMethod]
+        public void Test_Sorting_Incorrect_Format_Order_1()
+        {
+            IEnumerable<string> contents = new List<string> { "TERESSA, 88", "SMITH, ALLAN, 70", "SMITH, FRANCIS", "BUNDY, MADISON, 88" };
+            Assert.IsFalse(DemSort.Program.Sort(contents));                                                        
+        }
+    }
+}
